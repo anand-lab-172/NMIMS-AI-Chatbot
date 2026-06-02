8a75740044d520f47bdca9a9124db5f8f5e8e86a
Anandaram Ganapathi <74824247+anand-lab-172@users.noreply.github.com>
2026-06-02 12:38:41 +0530

    Update import for HuggingFaceEmbeddings module

--- src/ingest.py
+++ src/ingest.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from langchain_chroma import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 # ---------------- CONFIG ---------------- #
 
