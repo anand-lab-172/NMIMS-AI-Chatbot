27701d6c3df924856f73360650eafcd27af99504
Anandaram Ganapathi <74824247+anand-lab-172@users.noreply.github.com>
2026-06-02 15:10:53 +0530

    Refactor import statements in ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,10 +1,6 @@
-from langchain_community.vectorstores import (
-    Chroma
-)
+from langchain_community.vectorstores import Chroma
 
-from langchain_community.embeddings import (
-    HuggingFaceEmbeddings
-)
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 CHROMA_PATH = "chroma_db"
 
